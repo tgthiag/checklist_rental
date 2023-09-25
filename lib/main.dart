6ffa32b8c513bb0b546e35b789a2cf1f8f124489
tgthiag <tgthiag@gmail.com>
2023-09-25 11:10:59 -0300

    release 2, camera working

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,78 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: WebViewApp(),
-    ),
-  );
+final home_url =
+    Uri.parse('https://easyanalytics.com.br/appchecklist/index.html');
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.camera.request();
+  runApp(const MyApp());
 }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
-
-  @override
-  State<WebViewApp> createState() => _WebViewAppState();
-}
-
-class _WebViewAppState extends State<WebViewApp> {
-  late WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://easyanalytics.com.br/appchecklist/index.html'),
-      )
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (await controller.canGoBack()) {
-          await controller.goBack();
-          return false; // Prevent default navigation
-        }
-        return true; // Allow default navigation if cannot go back
-      },
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: SafeArea(
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
-                style: BorderStyle.solid,
-              ),
-              borderRadius: const BorderRadius.all(Radius.circular(5)),
-            ),
-            child: WebViewWidget(
-              controller: controller,
-            ),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      title: '',
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..addJavaScriptChannel('Toaster',
+        onMessageReceived: (JavaScriptMessage msg) {
+      var data = jsonDecode(msg.message);
+      if (data["cmd"] == "device") {
+        // Handle your JavaScript message here
+      }
+    })
+    ..loadRequest(home_url);
+
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+          mediaPlaybackRequiresUserGesture: false, javaScriptEnabled: true),
+      ios: IOSInAppWebViewOptions(
+          allowsInlineMediaPlayback: true,
+          allowsAirPlayForMediaPlayback: true));
+
+  InAppWebViewController? webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: InAppWebView(
+          initialUrlRequest: URLRequest(url: home_url),
+          initialOptions: options,
+          androidOnPermissionRequest: (InAppWebViewController controller,
+              String origin, List<String> resources) async {
+            return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT);
+          },
+          onWebViewCreated: (controller) async {
+            setState(() {
+              webViewController = controller;
+            });
+          },
+        )),
+      );
   }
 }
